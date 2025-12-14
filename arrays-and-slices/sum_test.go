852038d77fbfaca6ebe92c1a9f93df66a000756e
ryley <ryley_04@icloud.com>
2025-12-13 23:28:37 -0500

    slices progress

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -9,7 +9,7 @@ func TestSum(t *testing.T) {
 	numbers := [5]int{1, 2, 3, 4, 5}
 
 	got := Sum(numbers)
-	want := 14
+	want := 15
 
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
